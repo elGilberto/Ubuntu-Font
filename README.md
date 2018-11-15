c4db383c88cd7def3835fd36359751f4be9f3a8b
Gloeyisk <gloeyisk@gmail.com>
2018-11-15 22:02:26 +0700

    Hotfix
    Signed-off-by: Gloeyisk <gloeyisk@gmail.com>

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # Ubuntu Font
 This module is a simple way to apply Ubuntu font systemlessly. 
 
-## Recent Changes
-### v1.1
-- General optimization
-
-### v1.0
-- Initial release
-
 ## Source Code
 - Module [GitHub](https://github.com/gloeyisk/Ubuntu-Font)
--- config.sh
+++ config.sh
@@ -40,9 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "***********************"
+  ui_print " "
   ui_print "* Ubuntu Font Installer"
-  ui_print "***********************"
+  ui_print " "
 }
 
 ##########################################################################################
